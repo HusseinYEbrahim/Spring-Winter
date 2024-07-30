70925e0ca41aa2469695915eae137d50e5e6ff54
Hussein Ibrahim <hibrahim@sumerge.com>
2024-07-30 19:14:54 +0300

    Dep-Inj with annotations - Removed Beans config files - Configures Beans with @Component annotation - Made constructor dep-inj (auto-wiring) - Not having any conflicts yet, only one bean is in this commit.

--- courses/src/main/java/com/sumerge/courses/CoursesApplication.java
+++ courses/src/main/java/com/sumerge/courses/CoursesApplication.java
@@ -2,20 +2,24 @@ package com.sumerge.courses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class CoursesApplication {
 
+	static CourseRecommender courseRecommender;
+
+	CoursesApplication(CourseRecommender courseRecommender)
+	{
+		CoursesApplication.courseRecommender = courseRecommender;
+	} 
+
 	public static void main(String[] args) {
-		CourseRecommender courseRecommender;
 		/*
 		 * This is 1st appraoch for wiring the dependency 
 		 * 	by getting the Bean from the context and the Bean is configured in a config file
 		 *  and since we have two beans returning CourseRecommender then we have to choose them in getBean by name
 		 */
-		ApplicationContext ctx = SpringApplication.run(CoursesApplication.class, args);
-		courseRecommender = ctx.getBean("ratingRecommender", CourseRecommender.class);
+		SpringApplication.run(CoursesApplication.class, args);
 		System.out.println(courseRecommender.recommend());
 
 	}
