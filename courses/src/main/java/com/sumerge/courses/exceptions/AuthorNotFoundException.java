598202a1c06868f80838e154e994b33ae284475b
HusseinYasser <husseinyasser388@gmail.com>
2024-08-10 19:43:53 +0300

    added custom exceptions for error handling

---
+++ courses/src/main/java/com/sumerge/courses/exceptions/AuthorNotFoundException.java
@@ -0,0 +1,10 @@
+package com.sumerge.courses.exceptions;
+
+public class AuthorNotFoundException extends Exception{
+
+    public AuthorNotFoundException(String message)
+    {
+        super(message);
+    }
+    
+}
